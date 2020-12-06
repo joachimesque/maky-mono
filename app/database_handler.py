53ebfa51f3d945206b0d543a2bbc1a0271764c90
Joachim Robert <joachim.robert@protonmail.com>
2020-12-06 21:31:34 +0100

    Show/Edit message

--- app/database_handler.py
+++ app/database_handler.py
@@ -18,26 +18,64 @@ class Message(db.Entity):
     created_at = Required(datetime.datetime)
     published_at = Optional(datetime.datetime)
     message = Required(str, 1000)
+    tg_message_id = Required(int, size=32, index='message_id_index')
+    tg_date = Required(datetime.datetime, index='message_date_index')
 
 db.generate_mapping(create_tables=True)
 
 @db_session
-def add_message(message):
-    Message(message=message, created_at=datetime.datetime.now(datetime.timezone.utc))
-    return True
+def add_message(message, message_id, date):
+  if(not message or not message_id or not date):
+    return False
+
+  Message(message=message,
+          created_at=datetime.datetime.now(datetime.timezone.utc),
+          tg_message_id=message_id,
+          tg_date=date
+         )
+  return True
+
+@db_session
+def edit_message(message, message_id, date):
+  if(not message or not message_id or not date):
+    return False
+
+  message_obj = Message.get(tg_message_id = message_id)
+
+  if(not message_obj):
+    return False
+
+  message_obj.message = message
+  commit()
+  return True
+
+@db_session
+def get_message_by_id(message_id):
+  if(not message_id):
+    return False
+
+  message_obj = Message.get(tg_message_id = message_id)
+
+  return message_obj
+
+@db_session
+def get_last_message():
+  message = select(m for m in Message).order_by(desc(Message.created_at)).first()
+
+  return message
 
 @db_session
 def get_next_message():
-    all_messages = select(m for m in Message if m.published_at == None).order_by(Message.created_at)
-    
-    if(count(all_messages) > 0):
-      first_message = all_messages[:1][0]
-      return first_message
-    else:
-      return None
+  all_messages = select(m for m in Message if m.published_at == None).order_by(Message.created_at)
+  
+  if(count(all_messages) > 0):
+    first_message = all_messages[:1][0]
+    return first_message
+  else:
+    return None
 
 @db_session
 def invalidate_message(message_id):
-    message = Message.get(id=message_id)
-    message.published_at = datetime.datetime.now(datetime.timezone.utc)
-    commit()
+  message = Message.get(id = message_id)
+  message.published_at = datetime.datetime.now(datetime.timezone.utc)
+  commit()
--- app/telegram_handler.py
+++ app/telegram_handler.py
@@ -1,6 +1,10 @@
 from config import get_config
 from twitter_utils import tweet
-from database_handler import add_message
+from database_handler import (add_message,
+                              edit_message,
+                              get_message_by_id,
+                              get_next_message,
+                              get_last_message)
 
 import logging
 from telegram.ext import (Updater,
@@ -16,9 +20,9 @@ telegram_config = get_config()['telegram']
 
 # UTILS
 def check_user(user_id):
-  return user_id == 39069059
+  return user_id == telegram_config['restrict_user']
 
-def check_message(message, context = None, bot = None):
+def check_message(message, update = None, context = None):
   status_message = ''
 
   if(len(message) >= 500):
@@ -39,26 +43,76 @@ def start(update, context):
   response_text = 'Hello to you, too!'
   context.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
 
-def post_message(update, context):
+def show(update, context):
+  if(not context.args):
+    response_text = ("No argument given.\n"
+      "You can type '/show next', '/show last', "
+      "or any message number '/show 666'.")
 
-  if(check_user(update.message.from_user.id)):
-    check_message(update.message.text,
-                  update = update,
-                  context = context)
+  for arg in context.args:
+    message_obj = None
 
-    if(add_message(message=update.message.text)):
-      status_message = 'The message has been added to the queue!'
+    if(arg.isdigit()):
+      message_obj = get_message_by_id(message_id = arg)
+    elif(arg == 'last'):
+      message_obj = get_last_message()
+    elif(arg == 'next'):
+      message_obj = get_next_message()
+
+    if(message_obj):
+      response_text = message_obj.message
     else:
-      status_message = 'The message has not added to the queue :('
-  else:
-    status_message = 'I don’t know you!'
+      response_text = 'There is no message.'
+    
+  context.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
+
+
+
+def post_message(update, context):
+  if(update.message):
+    if(check_user(update.message.chat.id)):
+      check_message(update.message.text,
+                    update = update,
+                    context = context)
+
+      post_result = add_message(message=update.message.text,
+                                message_id= update.message.message_id,
+                                date= update.message.date)
+
+      if(post_result):
+        status_message = 'The message %s has been added to the queue!' % update.message.message_id
+      else:
+        status_message = 'The message has not added to the queue :('
+
+    else:
+      status_message = 'I don’t know you!'
+
+  elif(update.edited_message):
+    if(check_user(update.edited_message.chat.id)):
+      check_message(update.edited_message.text,
+                    update = update,
+                    context = context)
+
+      post_result = edit_message(message=update.edited_message.text,
+                                 message_id= update.edited_message.message_id,
+                                 date= update.edited_message.date)
+
+      if(post_result):
+        status_message = 'The message %s has been edited!' % update.edited_message.message_id
+      else:
+        status_message = 'The message has not been edited :('
+ 
+    else:
+        status_message = 'I don’t know you!'
   
   context.bot.send_message(chat_id=update.effective_chat.id, text=status_message)
 
 start_handler = CommandHandler('start', start)
+show_handler = CommandHandler('show', show)
 message_handler = MessageHandler(Filters.text & (~Filters.command), post_message)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(message_handler)
 
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(show_handler)
+dispatcher.add_handler(message_handler)
 
 updater.start_polling()
