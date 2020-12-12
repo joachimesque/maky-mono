5138941923f89637997e2228ee9fde4e484942e9
Joachim Robert <joachim.robert@protonmail.com>
2020-12-12 18:00:27 +0100

    Update message_utils.py

--- app/message_utils.py
+++ app/message_utils.py
@@ -6,9 +6,9 @@ def text_abuse_check(message_text):
     no_mentions = r"(^|\s)@[\w@.]+"
 
     if re.search(no_direct_message, message_text):
-        return(False, 'No direct message allowed')
+        return(False, 'No direct message allowed.')
 
     if re.search(no_mentions, message_text):
-        return(False, 'No mentions allowed')
+        return(False, 'No mentions allowed.')
 
-    return (True, 'All right')
+    return (True, 'All right.')
