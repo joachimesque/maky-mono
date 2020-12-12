c7a95ed5ca1956a17d24ce9425cb00e09be9ccbe
Joachim Robert <joachim.robert@protonmail.com>
2020-12-12 17:12:35 +0100

    Fix matching

--- app/message_utils.py
+++ app/message_utils.py
@@ -5,10 +5,10 @@ def text_abuse_check(message_text):
     no_direct_message = r"^d [\w]+"
     no_mentions = r"(?:^|[^\w])@[\w@.]+"
 
-    if re.match(no_direct_message, message_text):
+    if re.search(no_direct_message, message_text):
         return(False, 'No direct message allowed')
 
-    if re.match(no_mentions, message_text):
+    if re.search(no_mentions, message_text):
         return(False, 'No mentions allowed')
 
     return (True, 'All right')
