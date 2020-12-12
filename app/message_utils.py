ea73c01c381827b41e701832510899b51928dd56
Joachim <joachim.robert@protonmail.com>
2020-12-12 18:00:56 +0100

    Add abuse check (DM and Mentions) (#1)

---
+++ app/message_utils.py
@@ -0,0 +1,14 @@
+import re
+
+
+def text_abuse_check(message_text):
+    no_direct_message = r"^d [\w]+"
+    no_mentions = r"(^|\s)@[\w@.]+"
+
+    if re.search(no_direct_message, message_text):
+        return(False, 'No direct message allowed.')
+
+    if re.search(no_mentions, message_text):
+        return(False, 'No mentions allowed.')
+
+    return (True, 'All right.')
