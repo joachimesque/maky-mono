2c019c564f57d730b84ac7c81e90c3507c527ffd
Joachim Robert <joachim.robert@protonmail.com>
2020-12-12 17:12:43 +0100

    Fix regex

--- app/message_utils.py
+++ app/message_utils.py
@@ -3,7 +3,7 @@ import re
 
 def text_abuse_check(message_text):
     no_direct_message = r"^d [\w]+"
-    no_mentions = r"(?:^|[^\w])@[\w@.]+"
+    no_mentions = r"(^|\s)@[\w@.]+"
 
     if re.search(no_direct_message, message_text):
         return(False, 'No direct message allowed')
